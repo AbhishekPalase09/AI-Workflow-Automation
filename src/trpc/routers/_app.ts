31adc27b9f9a597f07fb985fcc9ace6113b0ec74
Aryan Mole <aryanmole@gmail.com>
2026-08-23 01:06:34 +0530

    Workflow crud setup

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,37 +2,10 @@ import { TRPCError } from "@trpc/server";
 import { inngest } from "@/inngest/client";
 import prisma from "@/lib/db";
 import { baseProcedure, createTRPCRouter, premiumProcedure, protectedProcedure } from "../init";
+import { workflowRouter } from "@/features/workflows/server/routers";
 
 export const appRouter = createTRPCRouter({
-  testAi: premiumProcedure.mutation(async () => {
-    // const { text } = await generateText({
-    //   model: google('gemini-2.5-flash'),
-    //   prompt: 'Write a vegetarian lasagna recipe for 4 people.',
-    // });
-
-    // throw new TRPCError({
-    //   code: "BAD_REQUEST",
-    //   message: "Something whent wrong",
-    // });
-
-    await inngest.send({
-      name: "execute/ai",
-    });
-
-    return { success: true, message: "Job queued" };
-  }),
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
-    return prisma.workflow.findMany();
-  }),
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: {
-        email: "mandar@gmail.com",
-      },
-    });
-    return { success: true, message: "Job queued" };
-  }),
+    workflows:workflowRouter
 });
 
 // export type definition of API
