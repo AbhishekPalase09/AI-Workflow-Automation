01050dc230a2df4cfdb069b4be3dfe1d227c36ba
Abhishek <abhishekpalase09@gmail.com>
2026-09-17 20:38:56 +0530

    21 node realtime

--- src/features/executions/types.ts
+++ src/features/executions/types.ts
@@ -1,3 +1,4 @@
+import type { Realtime } from "@inngest/realtime";
 import type { GetStepTools, Inngest } from "inngest";
 
 export type WorkflowContext = Record<string, unknown>;
@@ -11,7 +12,7 @@ export interface NodeExecutorParams<TData = Record<string, unknown>> {
   nodeId: string;
   context: WorkflowContext;
   step: StepTools;
-
+  publish: Realtime.PublishFn;
 };
 
 
--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,6 +1,7 @@
+import { realtimeMiddleware } from "@inngest/realtime/middleware";
 import {Inngest} from "inngest";
-
 // Create a client to send and recieve events
 export const inngest =new Inngest({
-    id:"nodebase"
+    id:"nodebase",
+    middleware: [realtimeMiddleware()],
 });
